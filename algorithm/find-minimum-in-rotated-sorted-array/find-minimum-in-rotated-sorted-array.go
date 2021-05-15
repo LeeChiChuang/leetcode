d7bc271f656f431284e4f10dcea96807648df5b3
lichichuang <lichichuang@9xiu.com>
2021-05-15 08:58:32 +0800

    add solution

--- algorithm/find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
+++ algorithm/find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.go
@@ -1,5 +1,7 @@
 package find_minimum_in_rotated_sorted_array
 
+import "fmt"
+
 func findMin(nums []int) int {
 	if len(nums) == 0 {
 		return -1
@@ -30,20 +32,44 @@ func findMin(nums []int) int {
 }
 
 func findMin2(nums []int) int {
+	fmt.Println("findMin2")
+	fmt.Println(nums)
 	n := len(nums)
 	l, r := 0, n-1
 
 	for l < r {
 		mid := l + r + 1>>1
+		fmt.Printf("l: %v, r: %v mid: %v, mVal: %v \n", l, r, mid, nums[mid])
 		if nums[mid] >= nums[0] {
 			l = mid
 		} else {
 			r = mid - 1
 		}
 	}
+	fmt.Printf("l: %v, r: %v \n", l, r)
+
 	if r+1 < n {
 		return nums[r+1]
 	} else {
 		return nums[0]
 	}
+}
+
+func findMin3(nums []int) int {
+	fmt.Println("findMin3")
+	fmt.Println(nums)
+	n := len(nums)
+	l, r := 0, n-1
+	for l < r {
+		mid := (l + r) >> 1
+		fmt.Printf("l: %v, r: %v mid: %v, mVal: %v \n", l, r, mid, nums[mid])
+		if nums[mid] > nums[r] {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+
+	fmt.Printf("l: %v, r: %v \n", l, r)
+	return nums[l]
 }
--- algorithm/find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array_test.go
+++ algorithm/find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array_test.go
@@ -34,11 +34,36 @@ func Test_findMin2(t *testing.T) {
 	}{
 		{name: "test1", args: args{nums: []int{3, 4, 5, 1, 2}}, want: 1},
 		{name: "test2", args: args{nums: []int{4, 5, 6, 7, 0, 1, 2}}, want: 0},
+		{name: "test3", args: args{nums: []int{4, 5, 6, 7, 8, 9, 10, 0, 1, 2}}, want: 0},
+		{name: "test4", args: args{nums: []int{11, 13, 15, 17}}, want: 11},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := findMin2(tt.args.nums); got != tt.want {
 				t.Errorf("findMin2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findMin3(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "test1", args: args{nums: []int{3, 4, 5, 1, 2}}, want: 1},
+		{name: "test2", args: args{nums: []int{4, 5, 6, 7, 0, 1, 2}}, want: 0},
+		{name: "test3", args: args{nums: []int{4, 5, 6, 7, 8, 9, 10, 0, 1, 2}}, want: 0},
+		{name: "test4", args: args{nums: []int{11, 13, 15, 17}}, want: 11},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMin3(tt.args.nums); got != tt.want {
+				t.Errorf("findMin3() = %v, want %v", got, tt.want)
 			}
 		})
 	}
